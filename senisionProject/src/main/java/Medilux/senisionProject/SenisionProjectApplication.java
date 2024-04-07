2ef320d58d1426508e3d00ba0c550e5786b3b857
Park-JuH <parkjuh2021@gmail.com>
2024-04-08 01:40:18 +0900

    initial project setup

---
+++ senisionProject/src/main/java/Medilux/senisionProject/SenisionProjectApplication.java
@@ -0,0 +1,13 @@
+package Medilux.senisionProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenisionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenisionProjectApplication.class, args);
+	}
+
+}
---
+++ senisionProject/src/test/java/Medilux/senisionProject/SenisionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Medilux.senisionProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenisionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
